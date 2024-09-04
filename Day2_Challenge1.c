93f22cddc1626acb8db6e0d95d7be93be5c55b4e
Saad el Quaul <saadelquaul@gmail.com>
2024-09-04 13:10:58 +0200

    Resolution de Challenge 1

---
+++ Day2_Challenge1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Challenge 1;
+int main()
+{
+    int TabValeurs[]={9,55,70,33,65};
+    for(int i=0;i<5;i++)
+    {
+        printf("Valeur %d = %d \n ",i+1,TabValeurs[i]);
+    }
+    
+    
+    return 0;
+}
